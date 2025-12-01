561da765c9f482c0898382fc3879c0412850a2f7
mbouchonnet <mathieubouchonnet@gmail.com>
2025-12-01 12:51:40 +0100

    support copilot and mistral for wks

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,16 +1,25 @@
+local function get_codecompanion_adapter()
+	local host = vim.uv.os_gethostname()
+	if host == "mathieu" then
+		return "mistral"
+	else
+		return "copilot"
+	end
+end
+
 return {
 	{
 
 		"olimorris/codecompanion.nvim",
 		tag = "v17.33.0",
 		enabled = function()
-			return vim.env.MISTRAL_API_KEY ~= nil
+			return vim.env.MISTRAL_API_KEY ~= nil or vim.env.COPILOT_API_KEY ~= nil
 		end,
 		opts = {
-			adapter = "mistral",
+			adapter = get_codecompanion_adapter(),
 			strategies = {
 				chat = {
-					adapter = "mistral",
+					adapter = get_codecompanion_adapter(),
 					keymaps = {
 						send = {
 							modes = { n = "<CR>", i = "<CR>" },
